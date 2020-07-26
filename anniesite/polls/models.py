a3a9050163e0b187eae9a160e914f546dbccf92b
anniesidd <annasiddle@msn.com>
2020-07-26 16:12:16 -0400

    first commit

---
+++ anniesite/polls/models.py
@@ -0,0 +1,46 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+"""
+models.Model is like defining a table
+models.CharField etc. are like defining columns
+- first arg is human-readable name, optional
+
+
+Equivalent SQL that runs when you run migrate
+(gotten by running 'python manage.py sqlmigrate polls 0001')
+
+
+BEGIN;
+--
+-- Create model Question
+--
+CREATE TABLE "polls_question" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "question_text" varchar(200) NOT NULL, "pub_date" datetime NOT NULL);
+--
+-- Create model Choice
+--
+CREATE TABLE "polls_choice" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "choice_text" varchar(200) NOT NULL, "votes" integer NOT NULL, "question_id" integer NOT NULL REFERENCES "polls_question" ("id") DEFERRABLE INITIALLY DEFERRED);
+CREATE INDEX "polls_choice_question_id_c5b4b260" ON "polls_choice" ("question_id");
+COMMIT;
+
+"""
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
